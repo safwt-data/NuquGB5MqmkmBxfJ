9637891d74ef047174fea55c3668bac4fb53021d
Safwt <80886399+safwt-data@users.noreply.github.com>
2026-08-20 17:03:14 +0200

    .

--- project_1/modeling/train.py
+++ project_1/modeling/train.py
@@ -7,16 +7,16 @@ with open("../models/project1/lr.pkl","wb") as file:
     pickle.dump(logreg, file)
 # SVM 
 with open("../models/project1/svm.pkl","wb") as file:
-    pickle.dump(logreg, file)
+    pickle.dump(svm, file)
 # Decision Tree
 with open("../models/project1/normal_tree.pkl","wb") as file:
-    pickle.dump(logreg, file)
+    pickle.dump(normal_tree, file)
 # Xgboost
 with open("../models/project1/xgboost.pkl","wb") as file:
-    pickle.dump(logreg, file)
+    pickle.dump(xgboost, file)
 # Random Forest
 with open("../models/project1/rf.pkl","wb") as file:
-    pickle.dump(logreg, file)
+    pickle.dump(rf, file)
 # Random Forest Tuned
 with open("../models/project1/rf_tuned.pkl","wb") as file:
-    pickle.dump(logreg, file)
+    pickle.dump(rf_tuned, file)
