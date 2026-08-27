edeefedd11a01205c2e2adac5970b0c7cb5fd45f
Safwt <80886399+safwt-data@users.noreply.github.com>
2026-08-27 16:36:25 +0200

    .

--- project_1/dataset.py
+++ project_1/dataset.py
@@ -5,13 +5,16 @@ import logging
 def load_data():
     logging.info("Loading dataset")
 
-    df = pd.read_csv("../data/ACME-HappinessSurvey2020.csv")
+    df = pd.read_csv("data\ACME-HappinessSurvey2020.csv")
 
     logging.info("Dataset loaded successfully")
 
     return df
 
 
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
 
+    df = load_data()
 
 
