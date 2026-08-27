86d6f4a80013cff556a7f97af95a9af7e7e3f267
Safwt <80886399+safwt-data@users.noreply.github.com>
2026-08-27 18:15:53 +0200

    .

--- project_1/modeling/train.py
+++ project_1/modeling/train.py
@@ -1,3 +1,4 @@
+import pickle
 import logging
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
@@ -54,5 +55,37 @@ if __name__ == "__main__":
     X_train, X_test, y_train, y_test = split_data(df)
 
     knn = KNeighborsClassifier(n_neighbors=20)
-
     trained_knn = train_model(knn, X_train, y_train)
+
+    logreg = LogisticRegression(random_state =42)
+    trained_logreg = train_model(logreg, X_train, y_train)
+
+    svm = SVC(random_state =42)
+    trained_svm = train_model(svm, X_train, y_train)
+
+    normal_tree = DecisionTreeClassifier(random_state=42,
+                                     class_weight='balanced')
+    trained_normal_tree = train_model(normal_tree, X_train, y_train)
+
+    xgboost = XGBClassifier(random_state =42)
+    trained_xgboost = train_model(xgboost,X_train,y_train)
+
+    rf = RandomForestClassifier(random_state=42,  
+                            class_weight='balanced')
+    trained_rf = train_model(rf,X_train,y_train)
+
+    tuned_rf = RandomForestClassifier(random_state=42,  
+                            class_weight='balanced',
+                            max_depth = 17,
+                            min_samples_leaf = 5,
+                            min_samples_split = 6,
+                            n_estimators = 675
+                           )
+    trained_tuned_rf = train_model(tuned_rf, X_train, y_train)
+
+
+
+    
+
+
+
